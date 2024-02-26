f737ba5a009bbc3b97978b0b2e80de0d8bac233c
Daniil <voloshchukd7@gmail.com>
2024-02-26 22:36:49 +0300

    flyway fix

--- src/main/resources/db/migration/V1__Init_DB.sql
+++ src/main/resources/db/migration/V1__Init_DB.sql
@@ -2,9 +2,10 @@ create table if not exists company (
     id bigserial not null,
     name varchar(255),
     primary key (id)
-)
+);
+
 create table if not exists employee (
     id bigserial not null,
     name varchar(255),
     primary key (id)
-)
+);
--- src/main/resources/db/migration/V3__Add_One_To_Many_Relation.sql
+++ src/main/resources/db/migration/V3__Add_One_To_Many_Relation.sql
@@ -1,3 +1,5 @@
+alter table if exists employee add column company_id bigint;
+
 ALTER TABLE employee
 ADD CONSTRAINT fk_company_id
 FOREIGN KEY (company_id) REFERENCES company(id);
